6da23f37554ad397b932d1ca7c7246c9405db712
PaoConcepcion <paolaconcepcion8@gmail.com>
2021-07-15 09:43:41 -0500

    Quite una tabla

--- citasApi/src/routes/privateRoutes/dates.js
+++ citasApi/src/routes/privateRoutes/dates.js
@@ -5,9 +5,11 @@ const mysqlConnection = require('../../database');
 
 router.get('/datesEmployee', (req, res) => {
     const id_empleado = req.id_empleado;
-    mysqlConnection.query(`select citas.id_cita, citas.fecha, citas.hora_entrada, citas.hora_salida, servicios.nombre
-        from citas, empleados_servicios, servicios 
+    mysqlConnection.query(`select citas.id_cita, citas.fecha, citas.hora_entrada, citas.hora_salida, 
+        concat(servicios.nombre, ' Cliente: ', usuarios.nombre, ' ', usuarios.apellido_paterno, ' ', correo, ' ', telefono) as nombre
+        from citas, empleados_servicios, servicios, usuarios
         where citas.id_empleado_servicio = empleados_servicios.id_empleado_servicio
+        and citas.id_usuario = usuarios.id_usuario
         and empleados_servicios.id_servicio = servicios.id_servicio
         and empleados_servicios.id_empleado = ${id_empleado};`, (err, rows, fields) => {
             if(!err){
--- controlcitas/src/app/components/service-client/service-client.component.ts
+++ controlcitas/src/app/components/service-client/service-client.component.ts
@@ -305,38 +305,32 @@ export class ServiceClientComponent implements OnInit {
         correo: userForm.correo,
         telefono: userForm.telefono
       };
-      const dataDate = {
-        id_cita: 0,
-        id_empleado_servicio: this.empleados.find(employee => employee.id_empleado ==  dateform.empleado).id_empleado_servicio,
-        fecha: dateform.dia,
-        hora_entrada: dateform.hora,
-        hora_salida: this.horaCitaSalida,
-      };
       this.citasApiService.alta(`/users`, dataUser).then((resUser) => {
+        const dataDate = {
+          id_cita: 0,
+          id_empleado_servicio: this.empleados.find(employee => employee.id_empleado ==  dateform.empleado).id_empleado_servicio,
+          fecha: dateform.dia,
+          hora_entrada: dateform.hora,
+          hora_salida: this.horaCitaSalida,
+          id_usuario: resUser[0][0].id_usuario,
+          costo: this.service.costo
+        };
         this.citasApiService.alta(`/dates`, dataDate).then((resDate) => {
-          const dataCitaEmpleado = {
-            id_cita_usuario: 0,
+          this.citaAgendada = true;
+          const correo = {
+            nombre: dataUser.nombre + ' ' + dataUser.apellido_paterno + ' ' + dataUser.apellido_materno,
+            correo: dataUser.correo,
+            telefono: dataUser.telefono,
             id_cita: resDate[0][0].id_cita,
-            id_usuario: resUser[0][0].id_usuario,
-            costo: this.service.costo
+            fecha: dataDate.fecha,
+            hora: dataDate.fecha,
+            servicio: this.service.nombre,
+            costo: dataDate.costo,
+            empleado:  this.empleados.find(employee => employee.id_empleado ==  dateform.empleado).nombre,
+            sucursal:  this.sucursales.find(laSucursal => laSucursal.id_sucursal ==  dateform.sucursal).nombre
           };
-          this.citasApiService.alta(`/dates_users`, dataCitaEmpleado).then(() => {
-            this.citaAgendada = true;
-            const correo = {
-              nombre: dataUser.nombre + ' ' + dataUser.apellido_paterno + ' ' + dataUser.apellido_materno,
-              correo: dataUser.correo,
-              telefono: dataUser.telefono,
-              id_cita: dataCitaEmpleado.id_cita,
-              fecha: dataDate.fecha,
-              hora: dataDate.fecha,
-              servicio: this.service.nombre,
-              costo: dataCitaEmpleado.costo,
-              empleado:  this.empleados.find(employee => employee.id_empleado ==  dateform.empleado).nombre,
-              sucursal:  this.sucursales.find(laSucursal => laSucursal.id_sucursal ==  dateform.sucursal).nombre
-            };
-            this.citasApiService.alta(`/send-email`, correo).then((res) => {
-              console.log(res);
-            });
+          this.citasApiService.alta(`/send-email`, correo).then((res) => {
+            console.log(res);
           });
         });
       });
