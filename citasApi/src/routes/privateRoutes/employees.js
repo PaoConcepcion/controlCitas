8dcb54fd4ced09a2e21bb759302d382d2daa3a2e
karinaG343 <karinaguerrero157@gmail.com>
2021-07-16 11:11:09 -0500

    cambios buscar empleados

--- citasApi/src/routes/privateRoutes/employees.js
+++ citasApi/src/routes/privateRoutes/employees.js
@@ -56,4 +56,26 @@ router.put('/changeRol/:id_empleado', (req, res) => {
     });
 }); 
 
+router.get('/employee-name/:nombre', (req, res) => {
+    const { nombre } = req.params;
+    mysqlConnection.query(`SELECT * FROM empleados WHERE nombre LIKE '%${nombre}%' and estatus = 1`, (err, rows, fields) => {
+        if(!err){
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+router.get('/employeesName/:busqueda', (req, res) => {
+    const { busqueda } = req.params;
+    mysqlConnection.query(`SELECT id_empleado, concat(nombre, ' ' ,apellido_paterno) as nombre FROM empleados WHERE concat(nombre, ' ' , apellido_paterno) like '%${busqueda}%' and estatus = 1;`, (err, rows, fields) => {
+        if(!err){
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
 module.exports = router;
--- citasApi/src/routes/privateRoutes/users.js
+++ citasApi/src/routes/privateRoutes/users.js
@@ -3,26 +3,4 @@ const router = express.Router();
 
 const mysqlConnection = require('../../database');
 
-router.get('/usersName', (req, res) => {
-    const { nombre } = req.body;
-    mysqlConnection.query(`SELECT id_empleado, concat(nombre, ' ' ,apellido_paterno) as nombre FROM empleados WHERE concat(nombre, ' ' , apellido_paterno) like '%${nombre}%';`, (err, rows, fields) => {
-        if(!err){
-            res.json(rows);
-        } else {
-            console.log(err);
-        }
-    });
-});
-
-router.get('/employee-name/:nombre', (req, res) => {
-    const { nombre } = req.params;
-    mysqlConnection.query(`SELECT * FROM empleados WHERE nombre LIKE '%${nombre}%' and estatus = 1`, (err, rows, fields) => {
-        if(!err){
-            res.json(rows);
-        } else {
-            console.log(err);
-        }
-    })
-});
-
 module.exports = router;
