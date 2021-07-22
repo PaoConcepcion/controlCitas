8bbee7d8353d99fc3ed6154080949b300aae0c6d
rodrigo.perez <190308@utags.edu.mx>
2021-07-22 10:07:30 -0500

    final ChatBot

--- citasApi/src/database.js
+++ citasApi/src/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'Rodri01tj',
     database: 'controlcitas',
     port: 3306,
     dateStrings: true
